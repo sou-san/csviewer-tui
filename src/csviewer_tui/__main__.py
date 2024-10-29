9b8e013680e7f47b8532484da1770e18853d6225
sou-san <sou887867@gmail.com>
2024-10-29 19:35:36 +0900

    Add src/csviewer_tui/__main__.py

---
+++ src/csviewer_tui/__main__.py
@@ -0,0 +1,26 @@
+import pathlib
+
+import click
+
+from csviewer_tui.app import Cvit
+
+
+def is_csv_file(
+    ctx: click.Context, param: click.Parameter, file_path: pathlib.Path
+) -> pathlib.Path:
+    if file_path.suffix.lower() == "csv":
+        return file_path
+
+    raise click.BadArgumentUsage(
+        f"{file_path} is invalid file.\nYou must specify a CSV file."
+    )
+
+
+@click.command()
+@click.argument(
+    "file_path",
+    type=click.Path(exists=True, readable=True, path_type=pathlib.Path),
+    callback=is_csv_file,
+)
+def main(file_path: pathlib.Path) -> None:
+    Cvit(file_path).run()
