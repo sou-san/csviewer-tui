d2152f4fde01c095416ffbaa3853e7dd10c15a59
sou-san <sou887867@gmail.com>
2024-10-29 21:56:51 +0900

    Add DataTable and key bindings in src/csviewer_tui/app.py

--- src/csviewer_tui/app.py
+++ src/csviewer_tui/app.py
@@ -1,19 +1,46 @@
+import csv
 import pathlib
 
 from textual.app import App, ComposeResult
+from textual.binding import Binding
 from textual.widgets import Footer, Header
+from textual_fastdatatable import ArrowBackend, DataTable, DataTableBackend
+
+
+class CsvFileDisplay(DataTable):
+    BINDINGS = [
+        Binding("k", "cursor_up", "Scroll Up"),
+        Binding("j", "cursor_down", "Scroll Down"),
+        Binding("h", "cursor_left", "Scroll Left"),
+        Binding("l", "cursor_right", "Scroll Right"),
+        Binding("g", "cursor_table_start", "Go to Top"),
+        Binding("G", "cursor_table_end", "Go to Bottom"),
+        Binding("b", "page_up", "Page Up"),
+        Binding("f", "page_down", "Page Down"),
+    ]
 
 
 class Cvit(App[None]):
     ENABLE_COMMAND_PALETTE = False
+    BINDINGS = [Binding("q", "exit_app", "Exit")]
 
     def __init__(self, file_path: pathlib.Path) -> None:
         self.file_path: pathlib.Path = file_path
         super().__init__()
 
     def compose(self) -> ComposeResult:
-        yield Header()
+        yield Header(icon="")
         yield Footer()
+        yield CsvFileDisplay(
+            backend=self._create_backend(), show_cursor=False, zebra_stripes=True
+        )
 
     def on_mount(self) -> None:
         self.title = str(self.file_path)
+
+    def _create_backend(self) -> DataTableBackend[str]:
+        with self.file_path.open(encoding="utf-8") as f:
+            return ArrowBackend.from_records(tuple(csv.reader(f)), has_header=True)
+
+    def action_exit_app(self) -> None:
+        self.exit()
